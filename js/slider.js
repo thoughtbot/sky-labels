0d6ec63017c63bd6a7fae65f7473cb14326be8ee
Paul Smith <catalystmediastudios@gmail.com>
2013-11-08 11:38:53 -0500

    Proof of Concept
    * Adds JavaScript
    * Styles labels

---
+++ js/slider.js
@@ -0,0 +1,23 @@
+$('body').on('focus', '.field-wrapper', function() {
+  var fieldWrapper = $(this);
+  var input = fieldWrapper.find('input').first();
+  if ( !fieldWrapperHasText(fieldWrapper) ) {
+    fieldWrapper.removeClass('js-slider-has-text');
+  };
+  fieldWrapper.addClass('js-slider-focused');
+});
+
+$('body').on('blur', '.field-wrapper', function() {
+  var fieldWrapper = $(this);
+  if ( fieldWrapperHasText(fieldWrapper) ) {
+    fieldWrapper.addClass('js-slider-has-text');
+  } else {
+    fieldWrapper.removeClass('js-slider-has-text');
+  };
+  fieldWrapper.removeClass('js-slider-focused');
+});
+
+function fieldWrapperHasText(fieldWrapper) {
+  var input = fieldWrapper.find('input').first();
+  return $.trim(input.val()).length
+}
